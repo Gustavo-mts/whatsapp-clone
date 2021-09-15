4582a919ce590162f56f8431a884b3ad935514e2
Gustavo Martins <ogustavo.developer@gmail.com>
2021-09-15 20:12:57 -0300

    subindo projeto

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-window">
+      <div className="sidebar">
+        ...
+      </div>
+      <div className="contentarea">
+        ...
+      </div>
+    </div>
+  );
+}
+
+export default App;
