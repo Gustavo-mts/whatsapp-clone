f1115934bb6d465195e4f23a1861f3273a656131
Gustavo Martins <gm357191@gmail.com>
2021-09-23 22:13:32 -0300

    config firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyAxprZtQOb-xwpEyqL7vC_PIwzkoaOK_-k",
+    authDomain: "whatsappclone-f4464.firebaseapp.com",
+    projectId: "whatsappclone-f4464",
+    storageBucket: "whatsappclone-f4464.appspot.com",
+    messagingSenderId: "249428333497",
+    appId: "1:249428333497:web:459465d9606060cf17dc68"
+  };
